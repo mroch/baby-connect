48aaf8f371a3ad8089a412a2a6cb6a49641f82c6
Androbot <andy@swings.net>
2017-09-23 13:10:20 -0700

    Add support for bottles and pumping. Updated request to match Android interface and include extra parameters needed for e.g. bottle amount.

--- src/request.js
+++ src/request.js
@@ -7,7 +7,7 @@ const https = require('https');
 const moment = require('moment');
 const querystring = require('querystring');
 
-const HOSTNAME = 'seacloud-2.appspot.com';
+const HOSTNAME = 'www.baby-connect.com';
 const LANGUAGE = 'en';
 
 class Request {
@@ -26,7 +26,7 @@ class Request {
       withDisable: 'true', // what does this mean?
       // pdt: TODO: YYMMDD, but is this necessary?
     });
-    return this.rawPost(`/CmdI?${qs}`, data);
+    return this.rawPost(`/CmdA?${qs}`, data);
   }
 
   // POST /CmdPostI?cmd=StatusMPost&lg=en HTTP/1.1
@@ -40,7 +40,7 @@ class Request {
   //   "e":"3/11/2017 16:15","Ptm":0
   // }]
   // &waccount2=1
-  saveStatus(user, kid, category, text, time) {
+  saveStatus(user, kid, category, text, time, params) {
     const dateStr = moment(time).format('YYMMDD');
     const timeStr = moment(time).format('HHmm');
     const data = querystring.stringify({
@@ -50,23 +50,24 @@ class Request {
       // tsn: 0000000000000000 TODO: last tsn received from previous response
       l: JSON.stringify([{
         Pdt: dateStr,
-        Ptm: 0,
+        Ptm: timeStr,
         Utm: timeStr,
-        Id: 0,
         Cat: category,
         By: user,
         Txt: text,
+        p: params,
         // lId: 0, TODO: local id? example: 220580527267
         Kid: kid,
-        e: moment(time).format('M/D/YYYY H:m')
+        e: moment(time).format('M/D/YYYY H:m'),
+        isst:true
       }]),
-      waccount2: 1 // what is this?
+      lg: LANGUAGE
     });
+    
     const qs = querystring.stringify({
       cmd: 'StatusMPost',
-      lg: LANGUAGE,
     });
-    return this.rawPost(`/CmdPostI?${qs}`, data);
+    return this.rawPost(`/CmdPostA?${qs}`, data);
   }
 
   rawPost(path, data) {
@@ -80,13 +81,12 @@ class Request {
         headers: {
           'Content-Type': 'application/x-www-form-urlencoded',
           'Content-Length': dataLength,
-          'BabyConnect': this.auth,
           'Authorization': `Basic ${this.auth}`,
           'User-Agent':
             // must impersonate the iOS app to get through (maybe iOS and
             // Android responses are different and it uses the UA?). trying to
             // be a good citizen by including our own identifier, though.
-            'Baby Connect 5.3.2i mroch-baby-connect/0.0.1'
+            'Baby Connect for Android 5.6.1a mroch-baby-connect'
         }
       };
 
